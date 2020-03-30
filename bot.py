74b3952d41e7af3c2e9d4786ed6ecdf0946fb5cb
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2020-03-30 22:40:26 +0800

    embed fix

--- bot.py
+++ bot.py
@@ -46,7 +46,8 @@ async def info(ctx):
     embed.add_field(name="Creator", value="Jonathan Tan", inline=True)
     embed.add_field(name="Version", value=VERSION, inline=True)
     embed.set_footer(text="I was created in my author's freetime. But he has no more, so don't expect much updates.")
-    #embed.set_thumbnail(url="./assets/img/icon.png")
+    icon = discord.File("assets/img/icon.png", filename="icon.png")
+    embed.set_thumbnail(url="attachment://icon.png")
     await ctx.send(embed=embed)
 
 @bot.event
