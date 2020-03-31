c61f65a75a97128838575a59ef22ec9fdba8773f
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2020-03-31 21:26:47 +0800

    update

--- fun.py
+++ fun.py
@@ -7,12 +7,13 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="roll", description="The number of dice rolled cannot exceed 500.", pass_context=True)
+    @commands.command(name="roll", description="The number of dice rolled cannot exceed 500. The type of dice rolled must be at least a d2 or at most a d100.", pass_context=True)
     async def roll(self, ctx, dice : str):
         """Rolls a dice in NdN format."""
         try:
             rolls, limit = map(int, dice.split('d'))
             if rolls > 500: raise commands.errors.UserInputError
+            if limit < 2 or limit > 100: raise commands.errors.UserInputError
         except Exception:
             raise commands.errors.UserInputError
 
@@ -32,11 +33,16 @@ class Fun(commands.Cog):
         if message.content.startswith("kids these days") or message.content.startswith("Kids these days"):
             await message.channel.send("ok boomer")
 
-    @commands.command(name="choose", pass_context=True)
+    @commands.command(name="choose", pass_context=True, description="Must be given at least 2 choices.")
     async def choose(self, ctx, *choices : str):
         """Chooses between multiple choices."""
+        if len(choices) < 2: raise commands.errors.UserInputError
         await ctx.message.delete()
-        await ctx.send(random.choice(choices))
+        embed=discord.Embed(title="Choice", description="For indecisive people.", color=0x32cd32)
+        embed.add_field(name="Choices", value=', '.join(choices), inline=False)
+        embed.add_field(name="Outcome", value=random.choice(choices), inline=False)
+        embed.set_footer(text="Fun Fact: You can put more than one word as an argument in any command that accepts strings. Surround the argument in double inverted commas \"like this\".")
+        await ctx.send(embed=embed)
 
     @commands.command(name='clap', description="You can clap from 0 to 10 claps.", pass_context=True)
     async def clap(self, ctx):
