0c08b81176669b3f2a69bc5f06ede54f07b3794d
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2020-03-31 08:42:09 +0800

    clapping

--- bot.py
+++ bot.py
@@ -15,20 +15,11 @@ bot = commands.Bot(command_prefix='^')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name='99', help="Responds with a random quote from Brooklyn 99")
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
+@bot.command(name='clap', help="A command to clap.")
+async def clap(ctx):
+    claps = random.randint(0,10)
     await ctx.message.delete()
-    await ctx.send(response)
+    await ctx.send(f'{ctx.author.mention} failed at clapping!' if claps == 0 else f'{ctx.author.mention} clapped! {":clap:" * claps}')
 
 @bot.command(name='roll_dice', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
